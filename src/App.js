406ca6522ca4c4e35d81e74f1a8f75afdda9c5fe
Carlos <c.baez1223@yahoo.com>
2021-05-24 22:51:29 -0400

    added a transition background for topbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react'
 import Topbar from "./components/topbar/Topbar"
 import Intro from "./components/intro/Intro"
 import Player from "./components/player/Player"
 import "./app.scss"
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+
   return (
     <div className="app">
-        <Topbar />
+        <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <div className="sections">
           <Intro />
           <Player /> 
